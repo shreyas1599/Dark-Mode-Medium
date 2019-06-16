6f2de7c0c453ffd0ee128730f32711b584c68f8b
Shreyas Thirumalai <shreyasthirumalai@gmail.com>
2019-06-16 08:55:21 +0530

    Added extension files

---
+++ Medium Dark Mode Extension/SafariExtensionViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SafariExtensionViewController.swift
+//  Medium Dark Mode Extension
+//
+//  Created by Shreyas Thirumalai on 6/15/19.
+//  Copyright © 2019 Shreyas Thirumalai. All rights reserved.
+//
+
+import SafariServices
+
+class SafariExtensionViewController: SFSafariExtensionViewController {
+    
+    static let shared: SafariExtensionViewController = {
+        let shared = SafariExtensionViewController()
+        shared.preferredContentSize = NSSize(width:320, height:240)
+        return shared
+    }()
+
+}
